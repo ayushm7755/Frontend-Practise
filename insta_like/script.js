8e16c795ee8bf04f27e09a0f5ebc587bb18b9b22
Ayush Mishra <ayushm7755@gmail.com>
2026-07-03 21:38:01 +0530

    like feature

--- insta_like/script.js
+++ insta_like/script.js
@@ -1,17 +1,16 @@
 var con = document.querySelector("#container");
-var img = document.querySelector("i");
+var love = document.querySelector("i");
 
 con.addEventListener("dblclick",()=>{
-    // console.log("hello");
-    img.style.transform = "translate(-50% , -50%) scale(1)";
-    img.style.opacity = 0.8;
-    img.style.color ="pink";
-
-    // setTimeout(()=>{
-    //     img.style.opacity =0.8;
-    // },100);
+    love.style.transform = "translate(-50% , -50%) scale(1)";
+    love.style.opacity = 0.8;
+    love.style.color ="pink";
 
     setTimeout(()=>{
-        img.style.transform = "translate(-50% , -50%) scale(0)";
-    },600);
+        love.style.opacity = 0;
+    },1000);
+
+    setTimeout(()=>{
+        love.style.transform = "translate(-50% , -50%) scale(0)";
+    },2000);
 })
