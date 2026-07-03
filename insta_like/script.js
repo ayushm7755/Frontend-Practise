1ea0375c293bc52cd40065514c44d3412d3f597f
Ayush Mishra <ayushm7755@gmail.com>
2026-07-03 17:31:33 +0530

    adding heart button

--- insta_like/script.js
+++ insta_like/script.js
@@ -2,5 +2,16 @@ var con = document.querySelector("#container");
 var img = document.querySelector("i");
 
 con.addEventListener("dblclick",()=>{
-    console.log("hello");
+    // console.log("hello");
+    img.style.transform = "translate(-50% , -50%) scale(1)";
+    img.style.opacity = 0.8;
+    img.style.color ="pink";
+
+    // setTimeout(()=>{
+    //     img.style.opacity =0.8;
+    // },100);
+
+    setTimeout(()=>{
+        img.style.transform = "translate(-50% , -50%) scale(0)";
+    },600);
 })
